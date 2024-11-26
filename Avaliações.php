6a7e424df72728c9a419a97f2f9cd531a2fd516e
Gi759 <andradeegiovanna.13@gmail.com>
2024-11-26 12:17:44 -0300

    Update and rename Avaliações.html to Avaliações.php

--- Avaliações.php
+++ Avaliações.php
@@ -1,3 +1,16 @@
+<?php
+
+$local_servidor = "localhost:3306";
+$bd_procurado = "bd_inter";
+
+$usuario = "root";
+$senha = "";
+
+$conexao_servidor_bd = mysqli_connect($local_servidor, $usuario, $senha,  $bd_procurado);
+                      
+
+?>
+
 <!DOCTYPE html>
 <html lang="Pt-Br">
 
