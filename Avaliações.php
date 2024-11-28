9c5da6bcc787f974aa43e30692ae69fcb924aa9e
fe976 <felipe.figueredo02@outlook.com>
2024-11-28 12:15:52 -0300

    Update Avaliações.php

--- Avaliações.php
+++ Avaliações.php
@@ -1,13 +1,22 @@
 <?php
 
-$local_servidor = "localhost:3306";
+$local_servidor = "localhost"; // Apenas "localhost"
 $bd_procurado = "bd_inter";
 
 $usuario = "root";
 $senha = "";
 
-$conexao_servidor_bd = mysqli_connect($local_servidor, $usuario, $senha,  $bd_procurado);
-                      
+// Conexão ao banco de dados
+$conexao_servidor_bd = mysqli_connect($local_servidor, $usuario, $senha, $bd_procurado, 3306);
+
+if (!$conexao_servidor_bd) {
+    die("Conexão falhou: " . mysqli_connect_error());
+}
+
+// Operações no banco de dados
+
+// Fechando a conexão
+mysqli_close($conexao_servidor_bd);
 
 ?>
 
