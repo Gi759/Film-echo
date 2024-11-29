18547cf8ac15991a75d33c728f6b4af7b51bf281
fe976 <felipe.figueredo02@outlook.com>
2024-11-29 02:10:39 -0300

    Update conexao_bd_mysql.php

--- database/conexao_bd_mysql.php
+++ database/conexao_bd_mysql.php
@@ -1,21 +1,17 @@
-<?php
+DROP DATABASE IF EXISTS `film_echo`;
 
-$local_servidor = "localhost"; // Apenas "localhost"
-$bd_procurado = "bd_inter";
+CREATE DATABASE IF NOT EXISTS `film_echo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `film_echo`;
 
-$usuario = "root";
-$senha = "";
+-- --------------------------------------------------------
 
-// Conexão ao banco de dados
-$conexao_servidor_bd = mysqli_connect($local_servidor, $usuario, $senha, $bd_procurado, 3306);
+-- Estrutura para tabela `tb_avaliacoes`
 
-if (!$conexao_servidor_bd) {
-    die("Conexão falhou: " . mysqli_connect_error());
-}
-
-// Operações no banco de dados
-
-// Fechando a conexão
-mysqli_close($conexao_servidor_bd);
-
-?>
+CREATE TABLE `tb_avaliacoes` (
+  `id_avaliacao` INT(10) NOT NULL AUTO_INCREMENT,
+  `poster_filme` VARCHAR(255) NOT NULL,
+  `nome_filme` VARCHAR(100) NOT NULL,
+  `avaliacao_texto` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `avaliacao_estrelas` DECIMAL(2,1) DEFAULT 0 CHECK (`avaliacao_estrelas` BETWEEN 0 AND 5),
+  PRIMARY KEY (`id_avaliacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
